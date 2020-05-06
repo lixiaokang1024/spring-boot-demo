5a21931e81fcaca3a8b812da1dd204cee2f9911c
lixiaokang <xiaokang_5@126.com>
2020-05-06 19:53:57 +0800

    compare sort demo

--- src/main/java/com/example/demo/Configuration.java
+++ src/main/java/com/example/demo/Configuration.java
@@ -3,13 +3,17 @@ package com.example.demo;
 import com.example.demo.aa.Cell;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Configuration {
   public static void main(String[] args) {
     String s1 = "TH0101";
     String s2 = "TH0102";
-    System.out.println("s1:" + s1.hashCode() + "s2:" + s2.hashCode());
+    System.out.println("s1:" + s1.hashCode() + ",s2:" + s2.hashCode());
+    char val[] = {'a', 1};
+    System.out.println(val[1] + 1);
     System.out.println(s1.hashCode() > s2.hashCode());
     List<Cell> cells = new ArrayList<>();
     cells.add(new Cell(1, 2));
@@ -18,6 +22,14 @@ public class Configuration {
     cells.add(new Cell(3, 2));
     testComparator(cells);
     System.out.println(cells);//[Cell [x=5, y=1], Cell [x=3, y=2], Cell [x=2, y=3]]
+
+    LinkedHashMap map = new LinkedHashMap<String, String>(10, 0.75f, true) {
+      //LinkedHashMap超出最大容量，删除head
+      @Override
+      protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+        return size() > 10;
+      }
+    };
   }
 
   public static void testComparator(List<Cell> cells) {
